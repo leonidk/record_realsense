450a0fc32bfeadf4ea2fdc322bb761b8166319c4
leonidk <lkeselman@gmail.com>
2016-10-06 11:23:31 -0700

    it works?

--- src/cpp-multicam.cpp
+++ src/cpp-multicam.cpp
@@ -21,6 +21,11 @@ void serialize( Archive & ar,rs::extrinsics & extrin)
 {
     ar( extrin.rotation, extrin.translation );
 }
+
+struct raw_header 
+{
+    int32_t width, height, channels, bytesperpixel;
+};
 int main(int argc, char * argv[]) try
 {
     std::string prefix_name("recording");
@@ -47,22 +52,29 @@ int main(int argc, char * argv[]) try
     
     // Enumerate all devices
     std::vector<rs::device *> devices;
-    std::vector<std::array<std::ofstream,2>> recordings;
-    for(int i=0; i<ctx.get_device_count(); ++i)
+    for(int i=0; i<ctx.get_device_count() && i <max_cameras; ++i)
     {
         devices.push_back(ctx.get_device(i));
     }
     auto stream_type_depth = rs::stream::depth_aligned_to_rectified_color;
     auto stream_type_color = rs::stream::rectified_color;
+    if(!using_rect) {
+        stream_type_depth = rs::stream::depth;
+        stream_type_color = rs::stream::color;
+    }
+    std::vector<std::pair<std::ofstream,std::ofstream>> recordings(devices.size());
     // Configure and start our devices
     for(int i=0; i < devices.size(); i++)
     {
         auto dev = devices[i];
 
         std::cout << "Starting " << dev->get_name() << "... ";
-        dev->enable_stream(rs::stream::depth, rs::preset::best_quality);
-        dev->enable_stream(rs::stream::color, rs::preset::best_quality);
+        dev->enable_stream(rs::stream::depth, z_w, z_h, rs::format::z16, 30);
+        dev->enable_stream(rs::stream::color, rgb_w, rgb_h, rs::format::rgb8, 30);
         dev->start();
+        dev->set_option(rs::option::r200_lr_auto_exposure_enabled,1);
+        dev->set_option(rs::option::color_enable_auto_exposure,1);
+        dev->set_option(rs::option::color_enable_auto_white_balance,1);
         std::cout << "done." << std::endl;
 
         {
@@ -79,6 +91,15 @@ int main(int argc, char * argv[]) try
             std::ofstream os(prefix_name + "_" + std::to_string(timestamp) + "_extrin_" +std::to_string(i) + ".json", std::ios::binary);
             cereal::JSONOutputArchive archive( os );
             archive(dev->get_extrinsics(stream_type_depth,stream_type_color));
+        }
+        {
+            recordings[i].first.open(prefix_name + "_" + std::to_string(timestamp) + "_depth_" +std::to_string(i) + ".rec", std::ios::binary);
+            recordings[i].second.open(prefix_name + "_" + std::to_string(timestamp) + "_color_" +std::to_string(i) + ".rec", std::ios::binary);
+            raw_header hd = {dev->get_stream_width(stream_type_depth), dev->get_stream_height(stream_type_depth), 1, 2};
+            recordings[i].first.write((char*)&hd,sizeof(raw_header));
+            raw_header hd2 = {dev->get_stream_width(stream_type_color), dev->get_stream_height(stream_type_color), 3, 1};
+            recordings[i].second.write((char*)&hd2,sizeof(raw_header));
+            
         }
     }
 
@@ -114,9 +135,19 @@ int main(int argc, char * argv[]) try
         glOrtho(0, w, h, 0, -1, +1);
         glPixelZoom(1, -1);
         int i=0, x=0;
-        for(auto dev : devices)
+        for(int i=0; i < devices.size(); i++)
         {
-            dev->poll_for_frames();
+            auto dev = devices[i];
+            dev->wait_for_frames();
+            {
+                uint32_t ts = std::time(0);
+                auto depth = dev->get_frame_data(stream_type_depth);
+                auto color = dev->get_frame_data(stream_type_color);
+                recordings[i].first.write((char*)&ts,sizeof(uint32_t));
+                recordings[i].first.write((char*)depth,z_w*z_h*2);
+                recordings[i].second.write((char*)&ts,sizeof(uint32_t));
+                recordings[i].second.write((char*)color,rgb_w*rgb_h*3);
+            }
             const auto c = dev->get_stream_intrinsics(rs::stream::color), d = dev->get_stream_intrinsics(rs::stream::depth);
             buffers[i++].show(*dev, rs::stream::color, x, 0, perTextureWidth, perTextureHeight);
             buffers[i++].show(*dev, rs::stream::depth, x, perTextureHeight, perTextureWidth, perTextureHeight);
